66b218ec1339ee189589f24f24a10d996f1215e6
waltli <waltli@live.com>
2018-06-20 18:16:06 +0800

    架构升级
    Signed-off-by: waltli <waltli@live.com>

---
+++ fetch-prj/src/main/java/com/sbolo/syk/fetch/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.sbolo.syk.fetch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jfinal.log.Log;
+import com.sbolo.syk.common.ui.RequestResult;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class TestController {
+	
+	@GetMapping("test111")
+	public String test(){
+		log.info("测试");
+		return null;
+	}
+}
