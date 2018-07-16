6f5cddf5f534012230371e78edf97c9d5ee8c4be
Walter <Walter@DESKTOP-1MJSU4O>
2018-07-16 23:21:54 +0800

    腾讯云对象桶配置

---
+++ fetch-prj/src/main/java/com/sbolo/syk/fetch/tool/BucketUtils.java
@@ -0,0 +1,33 @@
+package com.sbolo.syk.fetch.tool;
+
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.region.Region;
+
+public class BucketUtils {
+	
+	private static COSClient cosClient;
+	
+	public static COSClient instance() {
+		if(cosClient != null ) {
+			return cosClient;
+		}
+		// 1 初始化用户身份信息(secretId, secretKey)
+		COSCredentials cred = new BasicCOSCredentials("AKIDEPQGC0y7TTKBA4LD4mJfuyOjsrgQOhKm", "Ss9LNFvX1LUhsyulV6jR5e3HOTVLKof2");
+		// 2 设置bucket的区域, COS地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
+		ClientConfig clientConfig = new ClientConfig(new Region("ap-chengdu"));
+		// 3 生成cos客户端
+		COSClient cosclient = new COSClient(cred, clientConfig);
+		return cosclient;
+	}
+	
+	public static void shutdown() {
+		cosClient.shutdown();
+		cosClient = null;
+	}
+	
+	
+	
+}
