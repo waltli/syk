ae56aa06b499b3e2ba65ef703f9353e90e841a8a
Walter <Walter@DESKTOP-1MJSU4O>
2018-11-02 01:13:00 +0800

    评论系统

--- view-prj/src/main/webapp/assets/js/init.js
+++ view-prj/src/main/webapp/assets/js/init.js
@@ -3,3 +3,12 @@ $(function(){
 		effect:'fadeIn'
 	});
 });
+
+function strFmt(sources, fmtData){
+	for(var i in fmtData){
+		var keyRegx = new RegExp("\{%"+i+"\}", "g"); 
+		var value = fmtData[i];
+		sources = sources.replace(keyRegx, value);
+	}
+	return sources;
+}
