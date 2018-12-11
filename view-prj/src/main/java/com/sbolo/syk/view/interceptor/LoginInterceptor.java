1ba20b45142593aa8b458b254e4d0a79dcf87f18
waltli <waltli@live.com>
2018-12-11 18:52:47 +0800

    登录拦截器

--- view-prj/src/main/java/com/sbolo/syk/view/interceptor/LoginInterceptor.java
+++ view-prj/src/main/java/com/sbolo/syk/view/interceptor/LoginInterceptor.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -39,23 +40,67 @@ public class LoginInterceptor implements HandlerInterceptor {
 		HttpSession session = request.getSession();
 		Object user = session.getAttribute(CommonConstants.USER);
 		
-		if(user == null){
-//			SykUserVO sykUser = new SykUserVO();
-//			sykUser.setPrn("41877");
-//			sykUser.setAvatarUri("//qzapp.qlogo.cn/qzapp/101263695/E7F30E126A43785C2F97053AE2162341/100");
-//			sykUser.setNickname("qxw");
-//			session.setAttribute(CommonConstants.USER, sykUser);
-			
+		if(user != null){
+			return true;
+		}
+		
+		if(this.isXHR(request)) {
 			RequestResult<String> result = new RequestResult<>();
-			result.setCode(300);
+			result.setCode(HttpStatus.UNAUTHORIZED.value());
 			result.setError("请先登录。");
 			String json = JSON.toJSONString(result);
 			response.setContentType("application/json;charset=utf-8");
+			response.setStatus(HttpStatus.UNAUTHORIZED.value());
 			response.getWriter().write(json);
-			return false;
+		}else {
+			//获取请求的URL
+			String url = request.getRequestURI();
+			String contextPath = request.getContextPath();
+			String uri = url.replace(contextPath, "");
+			
+			Map<String, String[]> parameterMap = request.getParameterMap();
+			if(parameterMap != null && parameterMap.size() > 0) {
+				String params = "";
+				for(String key : parameterMap.keySet()) {
+					String[] strings = parameterMap.get(key);
+					String val = "";
+					for(String str :strings) {
+						val += ("," + str);
+					}
+					val = val.substring(1);
+					params  = params + key + "=" + val + "&";
+				}
+				if(StringUtils.isNotBlank(params)) {
+					params = "?"+params.substring(0, params.length()-1);
+					uri += params;
+				}
+			}
+			//跳转到登录界面
+			response.sendRedirect(contextPath+"/login?cb="+uri);
 		}
-		return true;
-//		
+		return false;
 	}
+	
+	private boolean isXHR(HttpServletRequest request) {
+        String accept = request.getHeader("accept");
+        if (accept != null && accept.indexOf("application/json") != -1) {
+            return true;
+        }
+
+        String xRequestedWith = request.getHeader("X-Requested-With");
+        if (xRequestedWith != null && xRequestedWith.indexOf("XMLHttpRequest") != -1) {
+            return true;
+        }
+
+        String uri = request.getRequestURI();
+        if (StringUtils.containsIgnoreCase(uri, ".xhr") || 
+        		StringUtils.containsIgnoreCase(uri, ".json") || 
+        		StringUtils.containsIgnoreCase(uri, ".xml")) {
+        	
+            return true;
+        }
+
+        return false;
+    }
 
 }
