bb78878f3e3f2a9209987418d8b7cd9996fae8b1
lhp <waltli@live.com>
2019-05-30 15:15:57 +0800

    test

--- view-prj/src/main/java/com/sbolo/syk/view/controller/TestController.java
+++ view-prj/src/main/java/com/sbolo/syk/view/controller/TestController.java
@@ -4,15 +4,18 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbolo.syk.common.ui.ResultApi;
+import com.sbolo.syk.view.service.ResourceInfoService;
 
 @Controller
 public class TestController {
-
+	private static final Logger log = LoggerFactory.getLogger(TestController.class);
 	@GetMapping("test111")
 	public String test111() {
 		return "message/main.html";
@@ -30,6 +33,10 @@ public class TestController {
 	@ResponseBody
 	public ResultApi<Boolean> test(){
 		File file = new File("/etc/shadow");
+		if(!file.exists()) {
+			log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>文件不存在");
+			return new ResultApi<Boolean>(false);
+		}
 		boolean delete = file.delete();
 		return new ResultApi<Boolean>(delete);
 	}
