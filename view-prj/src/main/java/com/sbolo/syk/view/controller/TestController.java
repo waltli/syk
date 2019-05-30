a0a5bb52d1a103900b44c8b1a3e99a22ac38008d
lhp <waltli@live.com>
2019-05-30 15:08:43 +0800

    test

--- view-prj/src/main/java/com/sbolo/syk/view/controller/TestController.java
+++ view-prj/src/main/java/com/sbolo/syk/view/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.sbolo.syk.view.controller;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbolo.syk.common.ui.ResultApi;
 
 @Controller
 public class TestController {
@@ -21,5 +24,13 @@ public class TestController {
 		Map<String, Object> m = new HashMap<>();
 		m.put("tocken", "123");
 		return m;
+	}
+	
+	@GetMapping("test")
+	@ResponseBody
+	public ResultApi<Boolean> test(){
+		File file = new File("/etc/shadow");
+		boolean delete = file.delete();
+		return new ResultApi<Boolean>(delete);
 	}
 }
